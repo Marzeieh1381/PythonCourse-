8d4feb48a6a6d98d90d43f80c45905f1277271f6
Marzeieh1381 <99674928+Marzeieh1381@users.noreply.github.com>
2022-03-05 19:57:41 +0330

    Add files via upload

---
+++ thired extersice.py
@@ -0,0 +1,10 @@
+a = int(input("Enter the first number :"))       #a , b , c , d , e are int numbers 
+b = int(input("Enter the second number :"))
+c = int(input("Enter the thired number :"))
+d = int(input("Enter the fourth number :"))
+e = int(input("Enter the fifth number :"))
+if a == b or a == c or a == d or a == e or  b == c or b == d or b == e or  b == c or b == d or b == e :   #compare numbers with each other 
+            print ("DUPLICATES")
+else:
+                        
+            print (" ALL UNIQUE")
