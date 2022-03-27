b38f9372d22aa00c2c18fb8fc1976355e6e3a8af
Marzeieh1381 <99674928+Marzeieh1381@users.noreply.github.com>
2022-03-27 20:36:16 +0430

    Add files via upload

---
+++ nouton - rafson.py
@@ -0,0 +1,17 @@
+
+#moadele = i**3 - i**2 +2
+x1 = 0
+x2 = 10
+for i in range(0, 11):
+    tabeasli = ((i*i*i)-(i*i)+2)
+    break
+for n in range (0,11):
+    moshtag = ((3*n*n)-(2*n)+2)
+    break
+for j in range (0,11):
+    kasr = tabeasli/moshtag
+    break
+for p in range (0, 11):
+    javab = p - kasr 
+    if javab < 0:
+        print (javab)
